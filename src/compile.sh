160c78c0b011905fffd19c791d7f25a80c5d5f2f
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-07-14 14:52:45 +0200

    Add extended ncurses library

--- src/compile.sh
+++ src/compile.sh
@@ -1,1 +1,1 @@
-gcc -Wall main.c -o main -lm -I/usr/include/pipewire-0.3 -I/usr/include/spa-0.2 -D_REENTRANT -lpipewire-0.3 -lncurses
+gcc -Wall main.c -o main -lm -I/usr/include/pipewire-0.3 -I/usr/include/spa-0.2 -D_REENTRANT -lpipewire-0.3 -lncursesw
